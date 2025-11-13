00ff6a9cc8cf0518c1f00eaafbd60b7820fd84d0
Sanskrati Agrawal <55913586+sanskrati-1234@users.noreply.github.com>
2025-11-13 22:34:39 +0530

    Update scroll-view.tsx

--- components/ui/scroll-view.tsx
+++ components/ui/scroll-view.tsx
@@ -1,11 +1,12 @@
 import { forwardRef } from 'react';
-import { ScrollView as RNScrollView, ScrollViewProps } from 'react-native';
+import { Platform, ScrollView as RNScrollView, ScrollViewProps } from 'react-native';
 
 export const ScrollView = forwardRef<RNScrollView, ScrollViewProps>(
   ({ style, ...otherProps }, ref) => {
     return (
       <RNScrollView
         ref={ref}
+        nestedScrollEnabled={Platform.OS === 'android' ? true : undefined}
         style={[{ backgroundColor: 'transparent' }, style]}
         {...otherProps}
       />
